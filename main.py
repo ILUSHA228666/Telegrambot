18a4463d371fdcfea91a3a7c6aaa3b10e155eee3
Ilya <ilya.gamer.yand@yandex.ru>
2023-01-14 19:10:10 +0300

    update

--- main.py
+++ main.py
@@ -1,28 +1,53 @@
 import logging
-import config as cf
-import aiogram as aio
-token = cf.token
-tgbot = aio.Bot(token)
-dp = aio.Dispatcher(tgbot)
+import config
+from aiogram import Bot, Dispatcher, executor, types
+ribov_list = [1,2,3,4,5,6,7,8,9]
+bot = Bot(token=config.token)
+dp = Dispatcher(bot)
 
 
-@dp.message_handler(commands = ['start'])
-async def send_welcome(message: aio.types.Message):
-    await message.reply("Привет!")
 
-@dp.message_handler(commands=['BB'])
-async def send_welcome(message: aio.types.Message):
-     await message.reply("Пока((")
 
-@dp.message_handler(commands=['lox'])
-async def send_welcome(message: aio.types.Message):
-    await message.reply("Ты лох))")
+HELP_MESSAGE = """
+<b>/help</b> - <em>список команд</em>
+<b>/start</b> - <em>старт бота</em>
+<b>/buy</b> - <em>купить рыбов</em>
+<b>/add</b> - <em>добавить рыбов</em>
+"""
 
-@dp.message_handler(commands=['NG'])
-async def send_welcome(message: aio.types.Message):
-    await message.reply("__/|\__   \n"
-                        "/ | \  \n")
+
+
+
+
+@dp.message_handler(commands=['buy'])
+async def send_bay_command(message: types.Message):
+    args = message.get_args()
+    if len(ribov_list) == 0:
+        return await bot.send_message(message.chat.id, "Мы сожалеем, " "но рыбов не осталось," "приходите по позже")
+    if not args:
+        return await bot.send_message(message.chat.id, f"Сейчас есть {len(ribov_list)} рыбов в наличии. Сколько рыбов вы хотите?")
+    else:
+        if args.isdigit():
+            args = int(args)
+            if args > len(ribov_list):
+                return await bot.send_message(message.chat.id, "Мы сожалеем, но рыбов не хватит, попробуйте ввести число поменьше")
+            else:
+                for i in range(args):
+                    #print(i)
+                    ribov_list.pop()
+                    return await bot.send_message(message.chat.id, f"Осталось {len(ribov_list)} рыбов в наличии")
+            else:
+                return await bot.send_message
+
+
+
+@dp.message_handler(commands = ['help'])
+async def send_welcome_command(message: types.Message):
+    return await bot.send_message(message.chat.id, HELP_MESSAGE, "HTML")
+
+
+
 
 if __name__ == '__main__':
-    aio.executor.start_polling(dp, skip_updates = True)
+    executor.start_polling(dp, skip_updates = True)
 
